a600a321f72933de2739963440f470f7a1618cba
Erick Sanchez <esericksanc@gmail.com>
2017-04-22 13:47:06 -0700

    added force controls

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    public float rotationSpeed;
+    public float thrustForce;
+
+    void FixedUpdate()
+    {
+
+        // Rotate the ship if necessary
+        transform.Rotate(0, 0, -Input.GetAxis("Horizontal") *
+            rotationSpeed * Time.deltaTime);
+
+        // Thrust the ship if necessary
+        GetComponent<Rigidbody2D>().
+            AddForce(transform.up * thrustForce *
+                Input.GetAxis("Vertical"));
+
+    }
+
+}
