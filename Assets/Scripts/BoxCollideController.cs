0dec9290ddda4f935c73e2b65771eb7f0c9f6386
AndrewE1234 <andrew@omnicode.com>
2017-04-22 16:38:58 -0700

    Added collision and the ship will now stop when it hits a planet

---
+++ Assets/Scripts/BoxCollideController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BoxCollideController : MonoBehaviour {
+   
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "basicWorld")
+        {
+            
+            Rigidbody2D rb = this.gameObject.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(0.0f, 0.0f);
+            PlayerControls.playerStop = true;
+            Debug.Log("Ouch! Fucker!");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public static bool playerStop;
+    public float rotationSpeed;
+    public float thrustForce;
+
+    private void Start()
+    {
+        playerStop = false;
+    }
+    void Update()
+    {
+        if(!playerStop)
+        {
+            // Rotate
+            transform.Rotate(0, 0, -Input.GetAxis("Horizontal") *
+                rotationSpeed * Time.deltaTime);
+
+            // Thrust
+            GetComponent<Rigidbody2D>().
+           AddForce(transform.up * thrustForce *
+               Input.GetAxis("Vertical"));
+        }
+        else
+        {
+            Rigidbody2D rb = this.gameObject.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(0.0f, 0.0f);
+            Debug.Log("This works, hurray!");
+        }
+    }
+}
