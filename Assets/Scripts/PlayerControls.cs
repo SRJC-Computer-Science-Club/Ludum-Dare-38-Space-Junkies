8afa6a09e28c791cafe9b340cd1881180ad3aded
Erick Sanchez <esericksanc@gmail.com>
2017-04-23 00:18:27 -0700

    Snap ships rotation to planet
    TODO: offset distance based on radius

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,22 +6,20 @@ public class PlayerControls : MonoBehaviour
 {
     public GameObject playerPrefab;
     public GameObject playerActual;
-    private static bool playerStop;
+    private static bool isLanded;
     public static bool moveMan;
-    private static bool stopSpawn;
     private float rotationSpeed = 100;
     public float thrustForce;
 
     
     private void Start()
     {
-        playerStop = false;
+        isLanded = false;
         moveMan = false;
-        stopSpawn = false;
     }
     void Update()
     {
-        if (!playerStop)
+        if (isLanded == false)
         {
             // Rotate
             transform.Rotate(0, 0, -Input.GetAxis("Horizontal") *
@@ -41,20 +39,30 @@ public class PlayerControls : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter2D(Collision2D coll)
+    void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.gameObject.tag == "basicWorld")
+        if (isLanded == false && coll.gameObject.tag == "basicWorld")
         {
             Rigidbody2D rb = this.gameObject.GetComponent<Rigidbody2D>();
             rb.velocity = new Vector2(0.0f, 0.0f);
-            this.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
-            playerStop = true;
 
-            if(stopSpawn == false)
-            {
-                playerActual = Instantiate(playerPrefab, new Vector3(this.transform.position.x, this.transform.position.y - 0.50f, 0.0f), Quaternion.identity);
-                stopSpawn = true;
-            }
+            float xCorr = coll.transform.position.x -this.transform.position.x;
+            float yCorr = coll.transform.position.y -this.transform.position.y;
+
+            var landingAngle = Mathf.Atan(yCorr / xCorr);
+
+            landingAngle *= 180f / Mathf.PI; //to degree
+
+            if (xCorr < 0)
+                landingAngle += 180;
+
+            this.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+
+            this.transform.Rotate(new Vector3(0f,0f,landingAngle +90));
+
+            playerActual = Instantiate(playerPrefab, new Vector3(this.transform.position.x, this.transform.position.y - 0.50f, 0.0f), Quaternion.identity);
+
+            isLanded = true;
         }
     }
 } 
