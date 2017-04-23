b7705a1526f1a793e1df89b55f32b222595a5179
Erick Sanchez <esericksanc@gmail.com>
2017-04-23 00:21:58 -0700

    temp

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -11,7 +11,7 @@ public class PlayerControls : MonoBehaviour
     private float rotationSpeed = 100;
     public float thrustForce;
 
-    
+
     private void Start()
     {
         isLanded = false;
@@ -46,8 +46,8 @@ public class PlayerControls : MonoBehaviour
             Rigidbody2D rb = this.gameObject.GetComponent<Rigidbody2D>();
             rb.velocity = new Vector2(0.0f, 0.0f);
 
-            float xCorr = coll.transform.position.x -this.transform.position.x;
-            float yCorr = coll.transform.position.y -this.transform.position.y;
+            float xCorr = coll.transform.position.x - this.transform.position.x;
+            float yCorr = coll.transform.position.y - this.transform.position.y;
 
             var landingAngle = Mathf.Atan(yCorr / xCorr);
 
@@ -58,11 +58,11 @@ public class PlayerControls : MonoBehaviour
 
             this.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
 
-            this.transform.Rotate(new Vector3(0f,0f,landingAngle +90));
+            this.transform.Rotate(new Vector3(0f, 0f, landingAngle + 90));
 
             playerActual = Instantiate(playerPrefab, new Vector3(this.transform.position.x, this.transform.position.y - 0.50f, 0.0f), Quaternion.identity);
 
             isLanded = true;
         }
     }
-} 
+}
