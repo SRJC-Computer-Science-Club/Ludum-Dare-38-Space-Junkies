b81fb17f422f37306baf9ccd344de92305d9f64c
HiimFinest <31751323+HiimFinest@users.noreply.github.com>
2017-09-29 13:27:45 -0700

    Modualer ship scene

---
+++ Assets/ModularShipControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModularShipControl : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject Body;
+    [SerializeField]
+    private GameObject LeftWing;
+    [SerializeField]
+    private GameObject RightWing;
+    [SerializeField]
+    private GameObject Thruster;
+
+    private float rotationSpeed = 100;
+    public float thrustForce;
+    // Use this for initialization
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+        GetComponent<Rigidbody2D>().AddForce(Input.GetAxis("Vertical") * transform.up * thrustForce);
+    }
+}
