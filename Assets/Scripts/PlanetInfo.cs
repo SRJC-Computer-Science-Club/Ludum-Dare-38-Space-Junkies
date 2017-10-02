ef9827f5bcafbbd359f01211110daacf55725c79
terrencedubois <31743872+terrencedubois@users.noreply.github.com>
2017-10-02 16:26:12 -0700

    planetInfo

---
+++ Assets/Scripts/PlanetInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetInfo : MonoBehaviour
+{
+    public float mass;
+    public int atmosphere;
+
+
+    void Start ()
+    {
+        mass = (this.transform.lossyScale.x * 25);
+        atmosphere = (int)(20 / this.transform.lossyScale.x);
+    }
+
+
+    public float getMass()
+    {
+        return mass;
+    }
+
+
+    public float getAtmosphere ()
+    {
+        return atmosphere;
+    }
+}
