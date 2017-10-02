ccedc660a5028606c0d60a952b0b6bbc6c952cee
ichiro <aussieevil@hotmail.com>
2017-10-02 15:53:39 -0700

    I think this works now

--- Assets/Scripts/SearchRing.cs
+++ Assets/Scripts/SearchRing.cs
@@ -6,23 +6,50 @@ public class SearchRing : MonoBehaviour {
     private Vector3 nearestPlanetCenter;
     private Vector3 distance;
     private bool inSpace;
-//    public GameObject
+    private GameObject ring;
+    private Transform ringTransform;
+    private GameObject[] planets;
+    private Transform[] planetTransforms;
+    private Vector3 ringAngle;
 
 	// Use this for initialization
 	void Start () {
         inSpace = false;
-	}
+        ringTransform = this.transform.GetChild(0);
+        ring = ringTransform.gameObject;
+        ring.SetActive(false);
+        planets = GameObject.FindGameObjectsWithTag("planet");
+        planetTransforms = new Transform[planets.Length];
+        int count = 0;
+        foreach (GameObject p in planets)
+        {
+            planetTransforms[count] = p.transform;
+            count++;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (PlayerControls.moveMan == true && GravitationalForces.totalForceReferance.magnitude > 10.0f)
         {
-
+            if (ring.activeSelf == true) ring.SetActive(false);
+        }
+        else
+        {
+            if (ring.activeSelf == false) ring.SetActive(true);
+            ringAngle = setAngle(calcAngle(getClosestPlanet(planetTransforms)) * -1);
+            ringTransform.rotation = Quaternion.Euler(ringAngle);
+            Debug.Log("Now my rotation should be" + ringAngle.z);
         }
 		
 	}
 
-    private float angle(Transform target)
+    private Vector3 setAngle(float input)
+    {
+        return new Vector3(0, 0, input);
+    }
+
+    private float calcAngle(Transform target)
     {
         Vector3 dir = target.position - this.transform.position;
         float result = Mathf.Atan(dir.x / dir.y) * Mathf.Rad2Deg;
