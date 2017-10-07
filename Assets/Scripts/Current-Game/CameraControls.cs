707642d4d229fd37d3b558127a0c1c98cb0685ef
Austin Meyer <meyerforge@gmail.com>
2017-10-07 14:18:33 -0700

    Player Combat
    - Set up code to start scene with only Major Tom in the scene.
    - Major Tom can now fire directional from the center of his body.
    - Imported testing laser.

--- Assets/Scripts/Current-Game/CameraControls.cs
+++ Assets/Scripts/Current-Game/CameraControls.cs
@@ -6,24 +6,41 @@ public class CameraControls : MonoBehaviour
 {
     public GameObject target;
     public GameObject dudeTarget;
+    public bool testMan;
 
     private Vector3 offset;
 
     // Use this for initialization
     void Start()
     {
-        offset = transform.position - target.transform.position;
+        if (!testMan)
+        {
+            offset = transform.position - target.transform.position;
+        }
+        else
+        {
+            Debug.Log("player is cameras target player");
+
+            offset = transform.position - dudeTarget.transform.position;
+        }
     }
 
     // Update is called once per frame but the last in line compared to void Update()
     void LateUpdate()
     {
-        if (!PlayerControls.moveMan)
+        if (!PlayerControls.moveMan && !testMan)
         {
             transform.position = target.transform.position + offset;
         }
+        else if (testMan)
+        {
+            Debug.Log("player is cameras target player");
+
+            transform.position = new Vector3 (dudeTarget.transform.position.x, dudeTarget.transform.position.y, -10.0f);
+        }
         else
         {
+            Debug.Log("player is cameras target player");
             dudeTarget = GameObject.FindGameObjectWithTag("Player");
 
             transform.position = new Vector3 (dudeTarget.transform.position.x, dudeTarget.transform.position.y, -10.0f);
