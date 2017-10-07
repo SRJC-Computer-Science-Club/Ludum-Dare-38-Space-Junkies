68319eabbca7b8347f9cd2574283dd7d14b1e56f
HiimFinest <31751323+HiimFinest@users.noreply.github.com>
2017-10-07 12:05:37 -0700

    HP slider and very rough script
    I added an HP slider on the UI because reasons and there is also a script I found that I modified to be a potential useful asset in the project... maybe. There isn't any functionality yet but I think it's worth looking into.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour 
+{
+    public int startingHealth = 100;                            // The amount of health the player starts the game with.
+    private int currentHealth;                                  // The current health the player has.
+    public Slider healthSlider;
+
+    bool isDead;                                                // Whether the player is dead.
+    bool damaged;                                               // True when the player gets damaged.
+
+    //public global::System.Int32 CurrentHealth { get = currentHealth; set => currentHealth => value; }
+    
+    void Awake()
+    {
+        // Setting up the references.
+        //anim = GetComponent<Animator>();
+        //playerAudio = GetComponent<AudioSource>();
+        //playerMovement = GetComponent<PlayerMovement>();
+        //playerShooting = GetComponentInChildren<PlayerShooting>();
+
+        // Set the initial health of the player.
+        currentHealth = startingHealth;
+    }
+
+
+    void Update()
+    {
+     /*   // If the player has just been damaged...
+        if (damaged)
+        {
+            // ... set the colour of the damageImage to the flash colour.
+            damageImage.color = flashColour;
+        }
+        // Otherwise...
+        else
+       // {
+            // ... transition the colour back to clear.
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        // Reset the damaged flag.
+        damaged = false; 
+        */
+    }
+
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        // Reduce the current health by the damage amount.
+        currentHealth -= amount;
+
+        // Set the health bar's value to the current health.
+        healthSlider.value = currentHealth;
+
+       
+
+        // If the player has lost all it's health and the death flag hasn't been set yet...
+        if (currentHealth <= 0 && !isDead)
+        {
+            // ... it should die.
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        // Set the death flag so this function won't be called again.
+        isDead = true;
+
+        // Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
+        //playerAudio.clip = deathClip;
+        //playerAudio.Play();
+
+        // Turn off the movement and shooting scripts.
+        //playerMovement.enabled = false;
+        //playerShooting.enabled = false;
+    }
+}
