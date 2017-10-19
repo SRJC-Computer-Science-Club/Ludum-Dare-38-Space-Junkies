5fc8b4e3e60ad2345127882671bfeff6cc905df7
AndrewE1234 <andrew@omnicode.com>
2017-10-18 17:22:16 -0700

    Added Drag Functionality

---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,40 @@
+﻿/*
+ The Drag Handler handles the dragging and placement of an inventory item.
+ This script should be placed on the image you wish to be dragged.
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler{
+
+    private Vector3 startPosition;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPosition = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 move = Input.mousePosition;
+        transform.position = new Vector3(move.x, move.y, transform.position.z);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = startPosition;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
