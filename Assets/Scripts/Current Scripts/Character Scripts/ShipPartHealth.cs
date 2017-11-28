b4fe55d0ee7a5e24a7a82c5fa4ff8fcad2e5b38a
AndrewE1234 <andrew@omnicode.com>
2017-11-27 16:56:59 -0800

    Started breakable ship parts

---
+++ Assets/Scripts/Current Scripts/Character Scripts/ShipPartHealth.cs
@@ -0,0 +1,36 @@
+﻿//Must be placed on a valid ship part that has
+//a ShipPart class attacked to it.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipPartHealth : MonoBehaviour {
+
+    private float currentHealth = 0;
+    private bool madeContact = false;
+
+	void Awake()
+    {
+        currentHealth = transform.GetComponent<ShipPart>().durability;
+	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(!madeContact)
+        {
+            currentHealth -= 5;
+            madeContact = true;
+            if (currentHealth <= 0)
+            {
+                transform.GetComponent<ShipPart>().thrustForse = 0;
+                transform.GetComponent<ShipPart>().destroyShipPart();
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
